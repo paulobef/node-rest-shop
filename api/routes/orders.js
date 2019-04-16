9937f5811e5cde8629053d04502dabc3f821fa50
paulobef <beffpa@gmail.com>
2019-04-17 00:28:29 +0200

    post orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order')
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -9,14 +12,21 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        product: req.body.productId,
         quantity: req.body.quantity
-    }
-    
-    res.status(201).json({
-        message: 'Order was placed',
-        order: order
+    });
+
+    order.save()
+    .then(result => {
+        res.status(201).json({
+            message: 'Order for ' + order.quantity + ' of Product ref°' + order.product + ' was placed',
+            order: result
+        });
+    })
+    .catch(err => {
+        res.status(500).json({ error : err });
     });
 });
 
