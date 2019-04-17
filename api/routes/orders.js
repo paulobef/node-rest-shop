441f4006f178eb16074e9501d847f68bdbf949fc
paulobef <beffpa@gmail.com>
2019-04-17 23:57:09 +0200

    get orders route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,9 +5,29 @@ const mongoose = require('mongoose');
 const Order = require('../models/order')
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
-        // here we will return a list of orders
+    Order.find()
+    .select('quantity _id product')
+    .exec()
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            orders: docs.map(doc =>  {
+                return {
+                    _id: doc._id,
+                    quantity: doc.quantity,
+                    product: doc.product,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/' + doc._id
+                    }
+                }
+            })
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            erros: err
+        })
     });
 });
 
